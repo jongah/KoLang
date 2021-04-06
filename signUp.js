9e88f853a7c7b58c72d0c245d4db497abcb0f04b
Anastacia-HS <kwon7788966@naver.com>
2021-04-06 20:52:40 +0900

    signup.js 정보 확인 및 패스워드 일치 확인

--- signUp.js
+++ signUp.js
@@ -1,11 +1,17 @@
 $(function() {
-  $('.login').click(function() {
+  $('.signup').click(function() {
+      var printNAME = $('.name').val();
       var printID = $('.id').val();
       var printPASS = $('.pass').val();
-      if (printID == '' || printPASS == '') {
+      var printPASSCK = $('.pass_check').val();
+      var printEMAIL = $('.email').val();
+
+      if (printNAME == '' || printID == '' || printPASS == '' || printPASSCK == '' || printEMAIL == '') {
           alert('입력되지 않은 항목이 있습니다.');
+      }else if(printPASS != printPASSCK) {
+        alert('비밀번호가 일치하지 않습니다.')
       }else {
-          alert('ID : ' + printID + '\nPASS : ' + printPASS);
+          alert('\nname : ' + printNAME + '\nID : ' + printID + '\nPASS : ' + printPASS + '\nPASS CHECK : ' + printPASSCK + '\nemail : ' + printEMAIL);
       }
   });
 });
