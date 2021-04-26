217d55bce0329fa6cd2ec9453005bab484096e0f
jongah <jongah2021@gmail.com>
2021-04-26 22:16:45 +0900

    #15 mongodb connextion ok

---
+++ db/app.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+// mongodb location
+mongoose.connect('mongodb://localhost:27017/spot'); 
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+	console.log("mongo db connection OK.");
+});
