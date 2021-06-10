c2355e2eaf7efb39c18c6e61363223c22622b670
jongah <jongah2021@gmail.com>
2021-06-11 04:34:15 +0900

    #44 구글 로그인 api 연동 및 세션 생성, 확인

--- api/models/user.js
+++ api/models/user.js
@@ -1,3 +1,44 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const contentSchema = new Schema({
+    c_name:{
+        type: String
+    },
+    text:{
+        type: String
+    },
+    w_score:{
+        type: Number
+    },
+    s_score:{
+        type: Number
+    }
+})
+
+const userSchema = new Schema({
+    id:{
+        type: String
+    },
+    accessToken:{
+        type: String
+    },
+    u_name:{
+        type: String
+    },
+    img:{
+        type: String, 
+        default: '../public/testImg.jpg'
+    },
+    color:{
+        type: Number
+    },
+    content:[contentSchema]
+});
+
+const User = mongoose.model('User', userSchema)
+module.exports = { User }
+
 // const mongoose = require("mongoose"); // mongoose 불러오기
 
 // // Schema 생성
