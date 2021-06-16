aa52e3b198137e516c93ccebc7bbb7271626214d
Anastasia <dbwjddbsgk@gmail.com>
2021-06-16 15:25:06 +0900

    profile 색 변경 태스트

--- js/modify_profile.js
+++ js/modify_profile.js
@@ -1,4 +1,5 @@
 function change(color) {
     // document.body.style.background=color;
     document.getElementById("modify").style.background=color;
+    document.getElementById("profile_box").style.background=color;
 }
