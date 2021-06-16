53a99bf365816b25143a5ac1901586eec26c49c7
suhyeun <ksh64520@naver.com>
2021-06-16 21:06:49 +0900

    profile page modal

---
+++ js/profile.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+  function onClick() {
+      document.querySelector('.modal').style.display ='block';
+      document.querySelector('.modal_bg').style.display ='block';
+  }   
+  function offClick() {
+      document.querySelector('.modal').style.display ='none';
+      document.querySelector('.modal_bg').style.display ='none';
+  }
+              
+  document.getElementById('.check_scorebtn').addEventListener('click', onClick);
+  document.querySelector('.close_btn').addEventListener('click', offClick);              
+};
