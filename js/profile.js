98296bf4779e075f74de7c5ba29ba1d55fdf5ab2
suhyeun <ksh64520@naver.com>
2021-06-18 08:18:43 +0900

    html 파일 ejs로 변환

--- js/profile.js
+++ js/profile.js
@@ -1,13 +1,23 @@
-window.onload = function() {
-  function onClick() {
-      document.querySelector('.modal').style.display ='block';
-      document.querySelector('.modal_bg').style.display ='block';
-  }   
-  function offClick() {
-      document.querySelector('.modal').style.display ='none';
-      document.querySelector('.modal_bg').style.display ='none';
+window.onload = function(){
+  const modal = document.getElementById("modal");
+  const modal_bg = document.getElementById("modal_bg");
+  const btnModals = document.getElementsByClassName("check_btn");
+
+  // 모달창 띄우기
+  for(var i = 0; i<btnModals.length; i++){
+    btnModals[i].addEventListener("click", e => {
+      modal.style.display = "flex";
+      modal_bg.style.display = "block";
+    });
   }
-              
-  document.getElementById('.check_scorebtn').addEventListener('click', onClick);
-  document.querySelector('.close_btn').addEventListener('click', offClick);              
+
+  // 모달창 이외의 영역을 클릭 시 종료
+  modal.addEventListener("click", e => {
+    const evTarget = e.target
+    if(evTarget.classList.contains("modal_area")) {
+        modal.style.display = "none"
+        modal_bg.style.display = "none";
+    }
+})
+
 };
